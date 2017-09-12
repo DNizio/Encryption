43b3d5452da181371eb8ea0696ba0b643af5a708
DNizio <30344561+DNizio@users.noreply.github.com>
2017-09-12 21:05:53 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,19 +5,21 @@ public class Main {
 
         Coding coding = new Coding();
         Decoding decoding = new Decoding();
-        String text="a";
+        byte correctChoice=0;
         do {
             System.out.println("[C}ode or [D]ecode?");
             Scanner scr = new Scanner(System.in);
-            text = scr.next();
+            String text = scr.next();
 
             if (text.toUpperCase().equals("C")) {
                 coding.coding();
+                correctChoice = 1;
             } else if (text.toUpperCase().equals("D")) {
                 decoding.decoding();
+                correctChoice = 1;
             } else
                 System.out.println("Try one more time by pressing C or D");
         }
-        while (text.toUpperCase().equals("C") || text.toUpperCase().equals("D"));
+        while (correctChoice == 0);
     }
 }
